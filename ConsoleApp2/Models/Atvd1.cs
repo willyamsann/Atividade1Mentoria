0cbef34e75b07140b0ef6c0c738f38b72d5fd3dc
willyamsann <willyam.oliveira@dellead.com>
2021-11-05 14:10:40 -0300

    Include new Methods

---
+++ ConsoleApp2/Models/Atvd1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    public class Atvd1
+    {
+        public double Media { get; set; }
+
+        public int IdadeMenor { get; set; }
+        public int IdadeMaior { get; set; }
+
+        public List<int> Idades { get; set; }
+
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ConsoleApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp2
 {
@@ -6,8 +9,97 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var nome = "TESTE";
+
+            bool loopContinue = true;
+            while (loopContinue)
+            {
+                Console.WriteLine("Menu de opçõs Mentoria");
+                Console.WriteLine("Escolha sua opção");
+                Console.WriteLine("1 - Atividade 1");
+                Console.WriteLine("2 - Atividade 2");
+                Console.WriteLine("3 - Atividade 3");
+                Console.WriteLine("Tecle qualquer numero diferente das opção acima para sair");
+                var option = Int32.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Opção 1");
+                        List<int> idades = new List<int>();
+                        var count = 5;
+                        for (var i = 0; i < count; i++)
+                        {
+                            Console.WriteLine("Digite a Idade da Pessoa Nª" + i + 1);
+
+                            var idade = Int32.Parse(Console.ReadLine());
+
+                            idades.Add(idade);
+                        }
+
+                        var calculateOne = CalculateAtvd1(idades);
+                        Console.WriteLine("Media: " + calculateOne.Media);
+                        Console.WriteLine("Idade Maior: " + calculateOne.IdadeMaior);
+                        Console.WriteLine("Idade Menor: " + calculateOne.IdadeMenor);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Opção 2");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Opção 3");
+                        break;
+
+                    default:
+                        loopContinue = false;
+                        Console.WriteLine("Default");
+                        break;
+                }
+
+            }
+
+
+
+
         }
+
+        public static Atvd1 CalculateAtvd1(List<int> idades)
+        {
+            var atividadeResult = new Atvd1();
+            atividadeResult.IdadeMaior = 0;
+            atividadeResult.IdadeMenor = 0;
+            atividadeResult.Idades = idades;
+            var somaIdades = 0;
+            foreach (var idade in idades)
+            {
+                somaIdades = somaIdades + idade;
+            }
+            atividadeResult.Media = somaIdades / 5;
+
+
+
+            foreach (var idade in idades)
+            {
+                if (idade >= atividadeResult.IdadeMaior)
+                {
+                    atividadeResult.IdadeMaior = idade;
+                }
+
+                if (atividadeResult.IdadeMenor == 0)
+                {
+                    atividadeResult.IdadeMenor = idade;
+                }
+
+
+                if (idade <= atividadeResult.IdadeMenor)
+                {
+                    atividadeResult.IdadeMenor = idade;
+                }
+            }
+
+            return atividadeResult;
+        }
+
+      
     }
 }
